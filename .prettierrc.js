5b845f1011fec2c87e8ebb77151c93538d623774
ZhangMin1998 <1464253423@qq.com>
2023-09-24 18:09:56 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // 强制使用单引号
+  singleQuote: true,
+  // 字符串使用单引号
+  singleQuote: true,
+  // 大括号内的首尾需要空格
+  bracketSpacing: true,
+  // 箭头函数参数括号
+  arrowParens: 'avoid',
+  // 在jsx中把'>' 是否单独放一行
+  jsxBracketSameLine: true,
+  // 换行符使用 crlf/lf/auto
+  endOfLine: 'auto'
+}
