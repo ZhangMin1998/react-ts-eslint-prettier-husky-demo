924c183e445a9a039638c4bd22dd38abdcf1ee70
ZhangMin1998 <1464253423@qq.com>
2023-09-26 22:23:46 +0800

    普通的JS变量，无法触发组件的更新

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,18 @@ import './App.css'
 import List1 from './List1'
 
 function App() {
+  let count = 0 // 普通的JS变量，无法触发组件的更新
+
+  const add = () => {
+    count++
+  }
   return (
-    <List1 />
+    <>
+      <List1 />
+      <div>
+        <button onClick={add}>{count}</button>
+      </div>
+    </>
   )
 }
 
