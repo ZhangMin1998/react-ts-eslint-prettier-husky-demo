a6766c8006c0d123fdb25486df17c09a3eeb2712
ZhangMin1998 <1464253423@qq.com>
2023-09-26 23:44:52 +0800

    state特点 不可变数据

--- src/StateDemo.tsx
+++ src/StateDemo.tsx
@@ -3,6 +3,7 @@ import React, { FC, useState } from 'react'
 const StateDemo: FC = () => {
 
   const [count, setCount] = useState(0)
+  const [userInfo, setUserInfo] = useState({ name: 'zm', age: 25 })
 
   const add = () => {
     // setCount(count + 1)
@@ -11,10 +12,17 @@ const StateDemo: FC = () => {
     // 如果一个变量 不用显示在页面，就用useRef管理
   }
 
+  const changeAge = () => {
+    setUserInfo({ name: 'zm11', age: 666 }) // 不可变数据 不去修改state值 而是传入一个新的值
+  }
+
   return (
     <>
       <div>
         <button onClick={add}>{count}</button>
+        <h2>不可变数据</h2>
+        <div>{JSON.stringify(userInfo)}</div>
+        <button onClick={changeAge}>change age</button>
       </div>
     </>
   )
@@ -26,3 +34,4 @@ export default StateDemo
 // state特点
 // 1、异步更新
 // 2、可能会被合并
+// 3、不可变数据
