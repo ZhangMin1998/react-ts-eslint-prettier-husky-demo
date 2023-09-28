d7375c880cbbea47ea2fe7db50c95c8f985960fa
ZhangMin1998 <1464253423@qq.com>
2023-09-29 00:15:02 +0800

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import './App.css'
 
 // import List1 from './List1'
 import List2 from './List2'
-import StateDemo from './StateDemo'
-import ImmerDemo from './ImmerDemo'
-import RefDemo from './RefDemo'
+import StateDemo from './useState'
+import ImmerDemo from './Immer'
+import RefDemo from './useRef'
+import UseMemoDemo from './useMemo'
 
 function App() {
   // let count = 0 // 普通的JS变量，无法触发组件的更新
@@ -25,6 +26,7 @@ function App() {
       <List2 />
       <ImmerDemo />
       <RefDemo />
+      <UseMemoDemo />
     </>
   )
 }
---
+++ src/useMemo.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useMemo, useState } from 'react'
+
+
+const UseMemoDemo: FC = () => {
+  console.log('1111111')
+  
+  const [num1, setNum1] = useState(10)
+  const [num2, setNum2] = useState(20)
+  const [text, setText] = useState('zm') // 更新 导致组件rerender
+
+  const sum = useMemo(() => {
+    console.log('gen  sum。。。。') // 缓存
+    
+    return num1 + num2
+  }, [num1, num2])
+
+  return (
+    <div>
+      <p>{sum}</p>
+      <p>
+        {num1}  <button onClick={() => setNum1(num1 + 1)}>num1++</button>
+      </p>
+      <p>
+        {num2}  <button onClick={() => setNum2(num2 + 1)}>num2++</button>
+      </p>
+      <div>
+        <input onChange={e => setText(e.target.value)} value={text}></input>
+      </div>
+    </div>
+  )
+}
+
+
+export default UseMemoDemo
