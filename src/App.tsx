c90a0bbdf3b49719a785313244ea9bf21b765982
ZhangMin1998 <1464253423@qq.com>
2023-09-29 23:15:21 +0800

    自定义hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// import React, { useState, useEffect } from 'react'
 import React, { useState } from 'react'
 import './App.css'
 // import type { MouseEvent } from 'react'
@@ -10,13 +11,25 @@ import RefDemo from './useRef'
 import UseMemoDemo from './useMemo'
 import UseCallbackDemo from './useCallback'
 
+import useTitle from './hooks/useTitle'
+
+// function useTitle(title: string) {
+//   useEffect(() => {
+//     document.title = title
+//   }, [])
+// }
+
 function App() {
+  useTitle('app page')
   // let count = 0 // 普通的JS变量，无法触发组件的更新
   const [count, setCount] = useState(0)
 
   const add = () => {
     setCount(count + 1)
   }
+  // useEffect(() => {
+  //   document.title = 'app page'
+  // }, [])
   return (
     <>
       {/* <List1 /> */}
