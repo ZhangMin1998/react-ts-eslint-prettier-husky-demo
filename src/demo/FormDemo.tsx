c6e3970bcde6f04e945bdbcd35d026fcea9e2142
zhangmin <zhangm@suntang.com>
2023-10-08 13:47:21 +0800

    使用textarea组件、dangerouslySetInnerHTML

--- src/demo/FormDemo.tsx
+++ src/demo/FormDemo.tsx
@@ -3,19 +3,36 @@ import React, { FC, useState, ChangeEvent } from 'react'
 
 const FormDemo: FC = () => {
 
-  // const val = 'hello'
-  const [text, setText] = useState<string>('hello')
+  // input
+  const [text, setText] = useState<string>('input')
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setText(event.target.value)
+  }
+
+  // textarea
+  const [text2, setText2] = useState<string>('textarea')
+  const handleChange2 = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText2(event.target.value)
+  }
+  const getHtml = () => {
+    return { __html: text2.replaceAll('\n', '<br>') }
   }
 
   return (
     <>
       <div>
         FormDemo:
-        <input type="text" value={text} onChange={handleChange}/>
-        <button onClick={() => console.log(text)}>打印</button>
-        <button onClick={() => setText('66666666')}>set</button>
+        <div>
+          <span>input:</span>
+          <input type="text" value={text} onChange={handleChange}/>
+          <button onClick={() => console.log(text)}>打印</button>
+          <button onClick={() => setText('66666666')}>set</button>
+        </div>
+        <div>
+          <span>textarea</span>
+          <textarea value={text2} onChange={handleChange2}/>
+          <p dangerouslySetInnerHTML={getHtml()}></p>
+        </div>
       </div>
     </>
   )
